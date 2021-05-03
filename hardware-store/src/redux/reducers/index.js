9db2503844a531b627ab9e3016d04711b30ea9bf
Your Name <you@example.com>
2021-05-03 20:37:27 +0300

    redux dark mode

--- hardware-store/src/redux/reducers/index.js
+++ hardware-store/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import cartReducer from './ReducerCart';
 import roleReducer from './ReducerRole';
+import darkModeReducer from './ReducerDarkMode';
 
 const rootReducer = combineReducers({
     cartReducer,
-    roleReducer
+    roleReducer,
+    darkModeReducer
 });
 
 export default rootReducer;
