249ae5742ec48984deef121e31d168394de7637a
Your Name <you@example.com>
2021-05-05 01:54:17 +0300

    add mysql connection

--- HardwareStoreServer/HardwareStoreServer/Models/ApplicationDbContext.cs
+++ HardwareStoreServer/HardwareStoreServer/Models/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using HardwareStoreServer.Models.DBModels;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,10 +13,15 @@ namespace HardwareStoreServer.Models
         {
             this.Database.EnsureCreated();
         }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //MS SQL Server
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-HO2E7N9;Initial Catalog=HardwareStoreDB;Trusted_connection=True");
+
+            //My SQL
+            /*optionsBuilder.UseMySql("server=localhost;user=root;password=1111;database=hardware_store;",
+                new MySqlServerVersion(new Version(8, 0, 23))
+            );*/
         }
         public virtual DbSet<Brand> Brands { get; set; }
         public virtual DbSet<Client> Clients { get; set; }
@@ -24,7 +30,7 @@ namespace HardwareStoreServer.Models
         public virtual DbSet<ProductOrderInfo> ProductOrderInfos { get; set; }
         public virtual DbSet<Supplier> Suppliers { get; set; }
         public virtual DbSet<Supply> Supplies { get; set; }
-        public virtual DbSet<Type> Types { get; set; }
+        public virtual DbSet<DBModels.Type> Types { get; set; }
         public virtual DbSet<Image> Images { get; set; }
     }
 }
